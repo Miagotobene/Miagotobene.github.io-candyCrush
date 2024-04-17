9ce2d50dd887ca856c68b56541b53ae76dbfac98
Miagotobene <miagotobene@gmail.com>
2024-04-17 15:10:13 -0700

    feat: added changes to code to make it work better

--- app.js
+++ app.js
@@ -1,220 +1,186 @@
-// creates array called 'candies' that contains all the colors of the candy pieces
-var candies = ["Blue", "Orange", "Green", "Yellow", "Red", "Purple"];
-
-// creates a 2D array called 'board' that holds all the img tags so that we can access them for crushing the candies and updating the img tags on the webpage
-const board = [];
-
-// creates rows and columns for our board
-var rows = 9;
-var columns = 9;
-
-// creates variable to keep track of the score--variable starts at 0
-let score = 0;
-
-// creates drag variables
-var currTile;
-var otherTile;
-
-window.onload = function() {
-    startGame();
-
-    //1/10th of a second
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const scoreDisplay = document.getElementById('score')
+    const width = 8
+    const squares = []
+    let score = 0
+    
+    const candyColors = [
+        'url(images/red-candy.png)',
+        'url(images/yellow-candy.png)',
+        'url(images/orange-candy.png)',
+        'url(images/purple-candy.png)',
+        'url(images/green-candy.png)',
+        'url(images/blue-candy.png)'
+      ]
+    
+    //create your board
+    function createBoard() {
+      for (let i = 0; i < width*width; i++) {
+        const square = document.createElement('div')
+        square.setAttribute('draggable', true)
+        square.setAttribute('id', i)
+        let randomColor = Math.floor(Math.random() * candyColors.length)
+        square.style.backgroundImage = candyColors[randomColor]
+        grid.appendChild(square)
+        squares.push(square)
+      }
+    }
+    createBoard()
+    
+    // Dragging the Candy
+    let colorBeingDragged
+    let colorBeingReplaced
+    let squareIdBeingDragged
+    let squareIdBeingReplaced
+    
+    squares.forEach(square => square.addEventListener('dragstart', dragStart))
+    squares.forEach(square => square.addEventListener('dragend', dragEnd))
+    squares.forEach(square => square.addEventListener('dragover', dragOver))
+    squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    squares.forEach(square => square.addEventListener('drageleave', dragLeave))
+    squares.forEach(square => square.addEventListener('drop', dragDrop))
+    
+    function dragStart(){
+        colorBeingDragged = this.style.backgroundImage
+        squareIdBeingDragged = parseInt(this.id)
+        // this.style.backgroundImage = ''
+    }
+    
+    function dragOver(e) {
+        e.preventDefault()
+    }
+    
+    function dragEnter(e) {
+        e.preventDefault()
+    }
+    
+    function dragLeave() {
+        this.style.backgroundImage = ''
+    }
+    
+    function dragDrop() {
+        colorBeingReplaced = this.style.backgroundImage
+        squareIdBeingReplaced = parseInt(this.id)
+        this.style.backgroundImage = colorBeingDragged
+        squares[squareIdBeingDragged].style.backgroundImage = colorBeingReplaced
+    }
+    
+    function dragEnd() {
+        //What is a valid move?
+        let validMoves = [squareIdBeingDragged -1 , squareIdBeingDragged -width, squareIdBeingDragged +1, squareIdBeingDragged +width]
+        let validMove = validMoves.includes(squareIdBeingReplaced)
+    
+        if (squareIdBeingReplaced && validMove) {
+            squareIdBeingReplaced = null
+        }  else if (squareIdBeingReplaced && !validMove) {
+           squares[squareIdBeingReplaced].style.backgroundImage = colorBeingReplaced
+           squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged
+        } else  squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged
+    }
+    
+    //drop candies once some have been cleared
+    function moveIntoSquareBelow() {
+        for (i = 0; i < 55; i ++) {
+            if(squares[i + width].style.backgroundImage === '') {
+                squares[i + width].style.backgroundImage = squares[i].style.backgroundImage
+                squares[i].style.backgroundImage = ''
+                const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+                const isFirstRow = firstRow.includes(i)
+                if (isFirstRow && (squares[i].style.backgroundImage === '')) {
+                  let randomColor = Math.floor(Math.random() * candyColors.length)
+                  squares[i].style.backgroundImage = candyColors[randomColor]
+                }
+            }
+        }
+    }
+    
+    
+    ///Checking for Matches
+    //for row of Four
+      function checkRowForFour() {
+        for (i = 0; i < 60; i ++) {
+          let rowOfFour = [i, i+1, i+2, i+3]
+          let decidedColor = squares[i].style.backgroundImage
+          const isBlank = squares[i].style.backgroundImage === ''
+    
+          const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
+          if (notValid.includes(i)) continue
+    
+          if(rowOfFour.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
+            score += 4
+            scoreDisplay.innerHTML = score
+            rowOfFour.forEach(index => {
+            squares[index].style.backgroundImage = ''
+            })
+          }
+        }
+      }
+      checkRowForFour()
+    
+    //for column of Four
+      function checkColumnForFour() {
+        for (i = 0; i < 39; i ++) {
+          let columnOfFour = [i, i+width, i+width*2, i+width*3]
+          let decidedColor = squares[i].style.backgroundImage
+          const isBlank = squares[i].style.backgroundImage === ''
+    
+          if(columnOfFour.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
+            score += 4
+            scoreDisplay.innerHTML = score
+            columnOfFour.forEach(index => {
+            squares[index].style.backgroundImage = ''
+            })
+          }
+        }
+      }
+    checkColumnForFour()
+    
+      //for row of Three
+      function checkRowForThree() {
+        for (i = 0; i < 61; i ++) {
+          let rowOfThree = [i, i+1, i+2]
+          let decidedColor = squares[i].style.backgroundImage
+          const isBlank = squares[i].style.backgroundImage === ''
+    
+          const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+          if (notValid.includes(i)) continue
+    
+          if(rowOfThree.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
+            score += 3
+            scoreDisplay.innerHTML = score
+            rowOfThree.forEach(index => {
+            squares[index].style.backgroundImage = ''
+            })
+          }
+        }
+      }
+      checkRowForThree()
+    
+    //for column of Three
+      function checkColumnForThree() {
+        for (i = 0; i < 47; i ++) {
+          let columnOfThree = [i, i+width, i+width*2]
+          let decidedColor = squares[i].style.backgroundImage
+          const isBlank = squares[i].style.backgroundImage === ''
+    
+          if(columnOfThree.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
+            score += 3
+            scoreDisplay.innerHTML = score
+            columnOfThree.forEach(index => {
+            squares[index].style.backgroundImage = ''
+            })
+          }
+        }
+      }
+    checkColumnForThree()
+    
+    // Checks carried out indefintely - Add Button to clear interval for best practise, or clear on game over/game won. If you have this indefinite check you can get rid of calling the check functions above.
     window.setInterval(function(){
-        crushCandy();
-        //resetScore();
-        slideCandy();
-        generateCandy();
-    });
-}
-
-// creates a random candy function that displays the candy images in a random fashion
-function randomCandy() {
-    return candies[Math.floor(Math.random() * candies.length)]; //0 - 5.99
-}
-
-// creates function to start game
-function startGame() {
-    for (let r = 0; r < rows; r++) {
-        let row = [];
-        for (let c = 0; c < columns; c++) {
-            // creates image tags:  <img id="0-0" src="./images/Red.png">
-            let tile = document.createElement("img");
-            tile.id = r.toString() + "-" + c.toString();
-            // sets source of image tag and creates a 'randomCandy'function (see above)
-            tile.src = "./images/" + randomCandy() + ".png";
-
-            // Creates drag functionality--event listeners
-            tile.addEventListener("dragstart", dragStart); //click on a candy, initialize drag process
-            tile.addEventListener("dragover", dragOver);  //clicking on candy, moving mouse to drag the candy
-            tile.addEventListener("dragenter", dragEnter); //dragging candy onto another candy
-            tile.addEventListener("dragleave", dragLeave); //leave candy over another candy
-            tile.addEventListener("drop", dragDrop); //dropping a candy over another candy
-            tile.addEventListener("dragend", dragEnd); //after drag process completed, we swap candies
-
-            // gets board div and append image tags unto board
-            document.getElementById("board").append(tile);
-            row.push(tile);
-        }
-        board.push(row);
-    }
-
-    console.log(board);
-}
-
-function dragStart() {
-    //this refers to the tile that was clicked on for dragging
-    currTile = this;
-}
-
-function dragOver(e) {
-    e.preventDefault();
-}
-
-function dragEnter(e) {
-    e.preventDefault();
-}
-
-function dragLeave() {
-
-}
-
-function dragDrop() {
-    //this refers to the target tile that was dropped on
-    otherTile = this;
-}
-
-function dragEnd() {
-
-    if (currTile.src.includes("blank") || otherTile.src.includes("blank")) {
-        return;
-    }
-
-    let currCoords = currTile.id.split("-"); // id="0-0" -> ["0", "0"]
-    let r = parseInt(currCoords[0]);
-    let c = parseInt(currCoords[1]);
-
-    let otherCoords = otherTile.id.split("-");
-    let r2 = parseInt(otherCoords[0]);
-    let c2 = parseInt(otherCoords[1]);
-
-    let moveLeft = c2 == c-1 && r == r2;
-    let moveRight = c2 == c+1 && r == r2;
-
-    let moveUp = r2 == r-1 && c == c2;
-    let moveDown = r2 == r+1 && c == c2;
-
-    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
-    if (isAdjacent) {
-        let currImg = currTile.src;
-        let otherImg = otherTile.src;
-        currTile.src = otherImg;
-        otherTile.src = currImg;
-
-        let validMove = checkValid();
-        if (!validMove) {
-            let currImg = currTile.src;
-            let otherImg = otherTile.src;
-            currTile.src = otherImg;
-            otherTile.src = currImg;    
-        }
-    }
-}
-
-// creates function for crushing candies
-function crushCandy() {
-    //crushFive();
-    //crushFour();
-    crushThree();
-    document.getElementById("score").innerText = score;
-
-}
-
-function crushThree() {
-    //check rows
-
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns-2; c++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r][c+1];
-            let candy3 = board[r][c+2];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                candy1.src = "./images/blank.png";
-                candy2.src = "./images/blank.png";
-                candy3.src = "./images/blank.png";
-                score += 10;
-            }
-        }
-    }
-
-    //check columns
-    for (let c = 0; c < columns; c++) {
-        for (let r = 0; r < rows-2; r++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r+1][c];
-            let candy3 = board[r+2][c];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                candy1.src = "./images/blank.png";
-                candy2.src = "./images/blank.png";
-                candy3.src = "./images/blank.png";
-                score += 10;
-            }
-        }
-    }
-}
-
-function checkValid() {
-    //check rows
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns-2; c++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r][c+1];
-            let candy3 = board[r][c+2];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                return true;
-            }
-        }
-    }
-
-    //check columns
-    for (let c = 0; c < columns; c++) {
-        for (let r = 0; r < rows-2; r++) {
-            let candy1 = board[r][c];
-            let candy2 = board[r+1][c];
-            let candy3 = board[r+2][c];
-            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-
-function slideCandy() {
-    for (let c = 0; c < columns; c++) {
-        let ind = rows - 1;
-        for (let r = columns-1; r >= 0; r--) {
-            if (!board[r][c].src.includes("blank")) {
-                board[ind][c].src = board[r][c].src;
-                ind -= 1;
-            }
-        }
-
-        for (let r = ind; r >= 0; r--) {
-            board[r][c].src = "./images/blank.png";
-        }
-    }
-}
-
-// function for generating new candies to replace the ones that fizzled out
-function generateCandy() {
-    for (let c = 0; c < columns;  c++) {
-        if (board[0][c].src.includes("blank")) {
-            board[0][c].src = "./images/" + randomCandy() + ".png";
-        }
-    }
-}
-
-// Issues to solve: Score doesn't reset at 0 because when the game is loaded, the board is randomly generated, and the code is constantly checking if candy pieces could be crushed. Therefore if the board loads with 3 candies in a row in some places, those will be crushed.
+        checkRowForFour()
+        checkColumnForFour()
+        checkRowForThree()
+        checkColumnForThree()
+        moveIntoSquareBelow()
+      }, 100)
+    })
